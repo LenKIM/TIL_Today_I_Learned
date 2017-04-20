06a8514e46271ba5eb72a97ff8527c46a16a12c6
LenKIM <joenggyu0@gmail.com>
2017-04-21 03:37:35 +0900

    —

--- Node/socket_chatting/socket.io.server.js
+++ Node/socket_chatting/socket.io.server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 //서버를 생성
 const http = require('http')
 const socketio = require('socket.io')
+var socket = require("events");
 
 
 const server = http.createServer((request, response) => {
@@ -20,10 +21,18 @@ server.listen(53147, () => {
 
 //소켓 서버를 만듬
 const io = socketio.listen(server);
+
 io.sockets.on('connection', (socket) => {
 
-//message이벤트
-socket.on('message', (data) => {
-  io.sockets.emit('message', data);
+    let roomName = null;
+    //join 이벤트
+    socket.on('join', (room) => {
+    roomName = room;
+    socket.join(room);
     });
+
+//message이벤트
+    socket.on('message', (data) => {
+      io.sockets.in(roomName).emit('message', data);
+        });
   });
