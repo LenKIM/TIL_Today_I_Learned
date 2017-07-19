81cd1e5e2023be5feccf41d7015095f8132f2e2b
LenKIM <joenggyu0@gmail.com>
2017-07-19 10:26:30 +0900

    반응형 웹 공부와 리액트 컴포넌트 주기 공부를 위한 레포

---
+++ 5.React/responsive/src/components/Slider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Slider extends React.Component {
+
+    render(){
+        return (
+          <section className="slider_section">
+              <span className="prev_btn">이전 버튼</span>
+              <span className="next_btn">다음 버튼</span>
+          </section>
+        )
+    }
+}
+
+export default Slider
